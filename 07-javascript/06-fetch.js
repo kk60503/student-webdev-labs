f2dbbb688af9e21e5b0e7f112a359e7581407b5a
Kyle Klein <132229861+kk60503@users.noreply.github.com>
2024-02-20 22:45:39 -0800

    completed 07-javascript

--- 07-javascript/06-fetch.js
+++ 07-javascript/06-fetch.js
@@ -1,9 +1,53 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
+
+const addBookToDom = (book) => {
+  let element = document.createElement("div");
+  let title = document.createElement("h3");
+  let author = document.createElement("p");
+  let released = document.createElement("p");
+  let pages = document.createElement("p");
+
+  title.textContent = book.name;
+  author.textContent = book.authors[0];
+  released.textContent = book.released.substr(0, 4);
+  pages.textContent = ` ${book.numberOfPages} pages`;
+
+  element.append(title);
+  element.append(author);
+  element.append(released);
+  element.append(pages);
+
+  element.style.display = "flex";
+  element.style.flexDirection = "column";
+  element.style.alignItems = "center";
+  element.style.marginTop = "20px";
+
+  app.append(element);
+};
 
 const fetchData = (url) => {
   // Fetch all books from the API of Ice and Fire and append them to the DOM
   // Create an element for each book that contains title, author, publication year, and number of pages
   // Update the styles in JavaScript to center all the books in the container given
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((book) => {
+        addBookToDom(book);
+      });
+    })
+    .catch((error) => {
+      let element = document.createElement("div");
+      element.textContent = "An error occured. Please reload the page.";
+      app.append(element);
+    })
+    .finally(() => {
+      let loader = document.querySelector("#loading");
+      app.removeChild(loader);
+    });
 };
+
+fetchData(url);
--- 07-javascript/07-functional.js
+++ 07-javascript/07-functional.js
@@ -1,41 +1,55 @@
 const characters = [
-  { id: 1, name: 'Arya', house: 'Stark' },
-  { id: 4, name: 'Sansa', house: 'Stark' },
-  { id: 16, name: 'Eddard', house: 'Stark' },
-  { id: 17, name: 'Catelyn', house: 'Stark' },
-  { id: 20, name: 'Robb', house: 'Stark' },
-  { id: 28, name: 'Rickon', house: 'Stark' },
-  { id: 40, name: 'Jaime', house: 'Lannister' },
-  { id: 41, name: 'Cersei', house: 'Lannister' },
-  { id: 42, name: 'Tyrion', house: 'Lannister' },
-  { id: 50, name: 'Tywin', house: 'Lannister' },
-  { id: 53, name: 'Kevan', house: 'Lannister' },
-  { id: 75, name: 'Daenerys', house: 'Targaryen' },
-  { id: 77, name: 'Viserys', house: 'Targaryen' },
-  { id: 81, name: 'Rhaegar', house: 'Targaryen' },
-  { id: 83, name: 'Theon', house: 'Greyjoy' },
-  { id: 84, name: 'Yara', house: 'Greyjoy' },
-  { id: 87, name: 'Davos', house: 'Seaworth' },
-  { id: 88, name: 'Brienne', house: 'Tarth' },
+  { id: 1, name: "Arya", house: "Stark" },
+  { id: 4, name: "Sansa", house: "Stark" },
+  { id: 16, name: "Eddard", house: "Stark" },
+  { id: 17, name: "Catelyn", house: "Stark" },
+  { id: 20, name: "Robb", house: "Stark" },
+  { id: 28, name: "Rickon", house: "Stark" },
+  { id: 40, name: "Jaime", house: "Lannister" },
+  { id: 41, name: "Cersei", house: "Lannister" },
+  { id: 42, name: "Tyrion", house: "Lannister" },
+  { id: 50, name: "Tywin", house: "Lannister" },
+  { id: 53, name: "Kevan", house: "Lannister" },
+  { id: 75, name: "Daenerys", house: "Targaryen" },
+  { id: 77, name: "Viserys", house: "Targaryen" },
+  { id: 81, name: "Rhaegar", house: "Targaryen" },
+  { id: 83, name: "Theon", house: "Greyjoy" },
+  { id: 84, name: "Yara", house: "Greyjoy" },
+  { id: 87, name: "Davos", house: "Seaworth" },
+  { id: 88, name: "Brienne", house: "Tarth" },
 ];
 
 const mapNameFamily = (characters) => {
   // Return an array with the name and house of each character
   // Format: "Arya, of House Stark"
 
-  return characters;
+  const namefamily = characters.map(
+    (item) => `${item.name}, of House ${item.house}`
+  );
+
+  return namefamily;
 };
 
 const filterFamily = (characters, house) => {
   // Return an array with only the characters from a given house
 
-  return characters;
+  const houses = characters.filter((item) => item.house === house);
+
+  return houses;
 };
 
 const reduceHouses = (characters) => {
   // Return an object with the number of characters from each house
 
-  return characters;
+  // const sum = characters.reduce((acc, curr) => acc + curr.id, 10000);
+
+  const houses = characters.reduce((acc, curr) => {
+    acc.hasOwnProperty(curr.house) ? acc[curr.house]++ : (acc[curr.house] = 1);
+
+    return acc;
+  }, {});
+
+  return houses;
 };
 
 console.log(mapNameFamily(characters));
@@ -60,7 +74,7 @@ console.log(mapNameFamily(characters));
 //   'Brienne, of House Tarth'
 // ]
 
-console.log(filterFamily(characters, 'Stark'));
+console.log(filterFamily(characters, "Stark"));
 // [
 //   { id: 1, name: 'Arya', house: 'Stark' },
 //   { id: 4, name: 'Sansa', house: 'Stark' },
@@ -70,7 +84,7 @@ console.log(filterFamily(characters, 'Stark'));
 //   { id: 28, name: 'Rickon', house: 'Stark' }
 // ]
 
-console.log(filterFamily(characters, 'Greyjoy'));
+console.log(filterFamily(characters, "Greyjoy"));
 // [
 //   { id: 83, name: 'Theon', house: 'Greyjoy' },
 //   { id: 84, name: 'Yara', house: 'Greyjoy' }
